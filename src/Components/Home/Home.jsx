656177ed663633306179bda46fc1257672e5c717
Ankit <ankitchauhan21500@gmail.com>
2023-07-09 10:49:07 +0530

    added new button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import firebaseConfig from '../../firebaseConfig';
+import bee from "./elem2.webp";
 export default function Home(props) {
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
@@ -26,6 +27,7 @@ export default function Home(props) {
     }
     return (
         <main className="main_home" id={props.id} >
+            <img style={{ width: "100px", height: "100px" }} className="floating_bee" src={bee} alt="" />
             <div>
                 <h1>
                     Your links
@@ -37,12 +39,13 @@ export default function Home(props) {
                     <p>
                         Welcome to our platform designed to empower you in the digital realm. Curate and showcase your online presence with ease, allowing others to explore the depths of your creativity. <b>Share you portfolio, social media accounts and more only with one ~ Link Bee ~ link.</b>
                     </p>
-                    <button onClick={handleNavigation} >Get Started</button>
+                    <div className="align2">
+                        <button onClick={handleNavigation} >Demo Page</button>
+                        <button onClick={() => { window.location.href = "/login" }} >Get Started</button>
+                    </div>
                 </div>
             </div>
-            <div>
-                <img onClick={() => { window.location.href = "http://linkbee.online/ankit21" }} src={HomeImg} alt="" />
-            </div>
+            <img onClick={() => { window.location.href = "http://linkbee.online/ankit21" }} src={HomeImg} alt="" />
         </main>
     )
 }
