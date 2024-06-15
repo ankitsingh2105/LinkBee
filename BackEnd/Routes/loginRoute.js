ae61498f962fe8a13c85efa81726344d0d76d0fd
Ankit <ankitchauhan21500@gmail.com>
2024-06-15 23:47:35 +0530

    testing deployment 1

--- BackEnd/Routes/loginRoute.js
+++ BackEnd/Routes/loginRoute.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 
         res.cookie('token', token, {
             secure: true, // Ensure your site uses HTTPS
-            httpOnly: true,
+            httpOnly: false,
             sameSite: 'Strict',
         });
         res.status(200).send({ token });
