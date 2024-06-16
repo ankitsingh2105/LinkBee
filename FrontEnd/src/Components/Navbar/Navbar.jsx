ae6adb23d2a313a06a8137aa60c5fd0b4ab7181e
Ankit <ankitchauhan21500@gmail.com>
2024-06-16 12:01:45 +0530

    testing deployment 2

--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar(props) {
 
     const handleLogout = async (e) => {
         try {
-            await axios.post("https://linkbee-2.onrender.com/user/logout");
+            await axios.post("/user/logout");
             // await axios.post("http://localhost:3000/user/logout");
             window.location.href = "/";
         }
@@ -50,7 +50,7 @@ export default function Navbar(props) {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios.get(`https://linkbee-2.onrender.com/user`,);
+                const response = await axios.get(`/user`,);
                 setname(response.data.name);
                 setuserID(response.data.userID);
                 setloading(false);
