eb21e7d11fa43e2db40d5c3b3232aaf401e0c481
AMANJEET <88767069+amanjeet21@users.noreply.github.com>
2024-09-20 19:19:07 +0530

    Update Footer.jsx

--- FrontEnd/src/Components/Footer/Footer.jsx
+++ FrontEnd/src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <h3>
                 ~ Start connecting with the world now ~
             </h3>
-            <small>Designed and developed by Amanjeet
+            <small>Designed and developed by <b>Amanjeet</b>
 {/*                 <a href="http://ankitsinghchauhan.in/">Ankit Singh Chauhan</a> */}
             </small>
         </main>
