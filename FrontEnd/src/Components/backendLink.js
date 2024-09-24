640a9e133a1e30be56ec2b27789aaf47aa9b8c42
Ankit <ankitchauhan21500@gmail.com>
2024-09-24 11:55:09 +0530

    new backEnd link

--- FrontEnd/src/Components/backendLink.js
+++ FrontEnd/src/Components/backendLink.js
@@ -1,3 +1,3 @@
 // const backendLink = "http://localhost:3000";
-const backendLink = "https://linkbee-2.onrender.com";
+const backendLink = "https://linkbee.onrender.com";
 export default  backendLink; 
