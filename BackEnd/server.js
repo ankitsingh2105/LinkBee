6661b7fef36fb013d98a2adbdc069b4c7c003038
Ankit <ankitchauhan21500@gmail.com>
2024-09-24 12:16:34 +0530

    fixed the env error

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -30,7 +30,7 @@ app.use(cors({
     origin: "https://linkbeemern.vercel.app",
     credentials: true 
 }));
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 
 
 const storage = multer.diskStorage({
